8eebeb94fb425a7c196b4d5b54079609146c0ad1
Bruno Pavlović <bpavlovic20@foi.hr>
2024-05-09 18:50:23 +0200

    Template for handling image upload

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,34 @@ server.get('/', (zahtjev, odgovor) => {
     odgovor.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
+server.post('/upload-image', async (zahtjev, odgovor) =>{
+    try {
+          const slikaTensor = await ImageHandler.loadImageToTensor(zahtjev.body.image);
+          const detektirajLice = await ImageHandler.detectFace(slikaTensor);
+      
+          if(detektirajLice != undefined){
+                await ImageHandler.cropImage(slikaTensor, detektirajLice);
+
+                //treniranje modela, predviđanje
+   
+                odgovor.status(200).json({
+                  success: true,
+                  message: 'OK' }
+                );
+          }
+      
+          odgovor.status(400).json({
+              success: false,
+              message: 'Lice nije pronađeno!' }
+          );
+    } catch (error) {
+        odgovor.status(500).json({
+            success: false,
+            message: 'Error' }
+        );
+    }
+});
+
 server.listen(port, () => {
     console.log(`Server pokrenut na portu: ${port}\n`);
     console.log("http://localhost:8050/");
