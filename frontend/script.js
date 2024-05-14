f310fb274f0c9542389a6b0746c2ee0604b03302
AndrejAntonic <andrej.antonic2@gmail.com>
2024-05-14 23:47:35 +0200

    Dodan tekst za prikaz odgovora.

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('input').addEventListener('change', function(event) {
+    const input = document.getElementById('input');
+    input.addEventListener('change', function(event) {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = function(e) {
+                input.disabled = true;
                 const image = document.getElementById('image');
+                const response = document.getElementById('response');
                 image.src = e.target.result;
                 image.style.display = 'block';
+
+                //Ovdje napraviti poziv na backend
+                //Sve ispod ovog neka se pozove tek kada se primi odgovor
+
+                response.textContent = ''; //Ovdje spremiti tekst odgovora
+                response.style.display = 'inline-block';
+
+                if(response.textContent == 'Real') {
+                    response.style.color = '#50C878';
+                } else if(response.textContent == 'DeepFake'){
+                    response.style.color = '#D21F3C';
+                } else {
+                    response.style.color = '#8B0000';
+                }
+
+                input.disabled = false;                
             };
             reader.readAsDataURL(file);
         }
