923292f2883b455afb198a14b5a1d069c850b9c3
BrunoPavlovic <bpavlovic20@foi.hr>
2024-05-14 18:46:46 +0200

    Upload image finished

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const path = require('path');
 const port = 8050;
 const server = express();
 
-const model = require('./model');
+const ImageHandler = require('./image-handler');
+const Model = require('./model');
+const upload = multer({ storage: multer.memoryStorage() });
 
 server.use(express.json());
 server.use(express.static(path.join(__dirname, '../frontend')));
@@ -12,38 +15,40 @@ server.get('/', (zahtjev, odgovor) => {
     odgovor.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-server.post('/upload-image', async (zahtjev, odgovor) =>{
+server.post('/upload-image', upload.single('image'),  async (zahtjev, odgovor) =>{
     try {
-          const slikaTensor = await ImageHandler.loadImageToTensor(zahtjev.body.image);
+          const slikaTensor = await ImageHandler.loadImageToTensor(zahtjev.file.buffer);
+          await ImageHandler.loadDetectionModel();
           const detektirajLice = await ImageHandler.detectFace(slikaTensor);
-      
           if(detektirajLice != undefined){
-                await ImageHandler.cropImage(slikaTensor, detektirajLice);
-                await model.load('./trained-model');
+                var cropped = await ImageHandler.cropImage(slikaTensor, detektirajLice);
+                
+                const model = new Model();
+                await model.load('file://trained-model/model.json');
 
-                var result = await model.predict(slikaTensor);
+                var result = await model.predict(cropped);
             
                 if(result == 'real'){
                     odgovor.status(200).json({
                         success: true,
                         message: 'Real' 
                     });
+                } else {
+                    odgovor.status(200).json({
+                        success: true,
+                        message: 'DeepFake' 
+                    });
                 }
-
-                odgovor.status(200).json({
-                    success: true,
-                    message: 'DeepFake' 
-                });
+          }else{
+            odgovor.status(400).json({
+                success: false,
+                message: 'Lice nije pronađeno!' }
+            );
           }
-      
-          odgovor.status(400).json({
-              success: false,
-              message: 'Lice nije pronađeno!' }
-          );
     } catch (error) {
         odgovor.status(500).json({
             success: false,
-            message: 'Error' }
+            message: 'Error' + error }
         );
     }
 });
